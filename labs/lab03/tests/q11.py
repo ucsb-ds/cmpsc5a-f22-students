da15bb044b7daaa69a0d6b577832352a0104e82c
ChristosZag <90874419+ChristosZag@users.noreply.github.com>
2022-10-11 22:51:25 -0700

    Add files via upload

---
+++ labs/lab03/tests/q11.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q11',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': ">>> woman_asking == 'The woman asked:'\n", 'hidden': False, 'locked': False},
+                                   {'code': '>>> gagarin_quote == \'"As a matter of fact, I have!"\'\n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ labs/lab03/tests/q31.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q31',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> type(top_10_movies) == tables.Table\n', 'hidden': False, 'locked': False},
+                                   {'code': ">>> top_10_movies.select('Rating', 'Name').sort('Name')\n", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
