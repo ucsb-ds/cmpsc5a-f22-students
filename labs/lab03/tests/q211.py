ff5d819b9fa5d84fdf4596ef049a483adb086df3
ChristosZag <90874419+ChristosZag@users.noreply.github.com>
2022-10-12 00:41:35 -0700

    Delete labs/lab03 directory

--- labs/lab03/tests/q211.py
+++
@@ -1,9 +0,0 @@
-test = {   'name': 'q211',
-    'points': None,
-    'suites': [   {   'cases': [   {'code': '>>> type(interesting_numbers) == np.ndarray\n', 'hidden': False, 'locked': False},
-                                   {'code': '>>> len(interesting_numbers) == 5\n', 'hidden': False, 'locked': False},
-                                   {'code': '>>> all(interesting_numbers == np.array([0, 1, -1, math.pi, math.e]))\n', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
--- labs/lab03/tests/q32.py
+++
@@ -1,9 +0,0 @@
-test = {   'name': 'q32',
-    'points': None,
-    'suites': [   {   'cases': [   {'code': '>>> type(imdb) == tables.Table\n', 'hidden': False, 'locked': False},
-                                   {'code': '>>> imdb.num_rows == 250\n', 'hidden': False, 'locked': False},
-                                   {'code': ">>> imdb.select('Votes', 'Rating', 'Title', 'Year', 'Decade').sort(0).take(range(2,5))\n", 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
